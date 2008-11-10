6cb09f6da6fd85216a1037a0646354905cf6e217
Alex Pardoe <digital.pardoe@gmail.com>
2008-11-10 22:35:30 +0000

    First commit of base Xcode project.

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  Warblr
+//
+//  Created by Alex on 10/11/2008.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Warblr
+//
+//  Created by Alex on 10/11/2008.
+//  Copyright __MyCompanyName__ 2008 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
