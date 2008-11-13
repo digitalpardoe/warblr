47350e9aea3588f4571477606d1f1e7c00b2e34c
Alex Pardoe <digital.pardoe@gmail.com>
2008-11-13 20:45:44 +0000

    Added more necessary classes to the project.

---
+++ DPGrowl.m
@@ -0,0 +1,44 @@
+//
+//  DPGrowl.m
+//
+//  Created by Alex on 31/05/2008.
+//  Copyright 2008 digital:pardoe. All rights reserved.
+//
+
+#import "DPGrowl.h"
+
+@implementation DPGrowl
+
+static DPGrowl *singleton;
+
++ (DPGrowl *)theGrowl
+{
+	if (!singleton)
+		singleton = [[DPGrowl alloc] init];
+	
+	return singleton;
+}
+
++ (id)alloc
+{
+	singleton = [super alloc];
+	return singleton;
+}
+
+- (void)initializeGrowl
+{
+	[GrowlApplicationBridge setGrowlDelegate:@""];
+}
+
+- (void)showGrowlNotification:(NSString *)growlName:(NSString *)growlTitle:(NSString *)growlDescription
+{
+	[GrowlApplicationBridge notifyWithTitle:growlTitle
+								description:growlDescription
+						   notificationName:growlName
+								   iconData:nil
+								   priority:0
+								   isSticky:NO
+							   clickContext:nil ];
+}
+
+@end
