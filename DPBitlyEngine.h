9c4dae9415f97d683d7e801cf15fa7b9f8ecaaef
Alex Pardoe <digital.pardoe@gmail.com>
2008-12-24 22:45:40 +0000

    Added blank files to start bit.ly integration.

---
+++ DPBitlyEngine.h
@@ -0,0 +1,16 @@
+//
+//  DPBitlyEngine.h
+//  Warblr
+//
+//  Created by Alex on 21/12/2008.
+//  Copyright 2008 digital:pardoe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DPBitlyEngine : NSObject {
+
+}
+
+@end
