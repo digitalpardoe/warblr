9118087b5c2311208bc3652e974782a1fe7388a6
digitalpardoe <digital.pardoe@gmail.com>
2009-04-10 11:53:16 +0100

    Added a manager class for getting and processing tweets.

---
+++ TweetManager.h
@@ -0,0 +1,16 @@
+//
+//  TweetManager.h
+//  Warblr
+//
+//  Created by Alex on 09/04/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TweetManager : NSObject {
+
+}
+
+@end
---
+++ TweetManager.m
@@ -0,0 +1,14 @@
+//
+//  TweetManager.m
+//  Warblr
+//
+//  Created by Alex on 09/04/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "TweetManager.h"
+
+
+@implementation TweetManager
+
+@end
